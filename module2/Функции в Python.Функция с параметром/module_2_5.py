450bacaeb511c06c246ca7d85d0335b62603c848
timur.guliev <timur.guliev@gmail.com>
2024-07-13 22:16:12 +0500

    add module_2_5

---
+++ module2/Функции в Python.Функция с параметром/module_2_5.py
@@ -0,0 +1,22 @@
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        new_row = []
+        matrix.append(new_row)
+        for a in range(m):
+            new_row.append(value)
+    return matrix
+
+def main():
+    print(get_matrix(3, 5, 10))
+    
+    result1 = get_matrix(2, 2, 10)
+    result2 = get_matrix(3, 5, 42)
+    result3 = get_matrix(4, 2, 13)
+    print(result1)
+    print(result2)
+    print(result3)
+
+if __name__ == "__main__":
+    main()
+    
