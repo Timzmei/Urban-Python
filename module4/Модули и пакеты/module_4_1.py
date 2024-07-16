bf86e9fb22e8d9c90c18c5bdb549f4e096534fd4
timur.guliev <timur.guliev@gmail.com>
2024-07-16 21:12:35 +0500

    add module_4_1

---
+++ module4/Модули и пакеты/module_4_1.py
@@ -0,0 +1,17 @@
+from fake_math import divide as fm_d
+from true_math import divide as tm_d
+
+
+
+def main():  
+    result1 = fm_d(69, 3)
+    result2 = fm_d(3, 0)
+    result3 = tm_d(49, 7)
+    result4 = tm_d(15, 0)
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
+        
+if __name__ == "__main__":
+    main()
