edd2b17b373e3b65d876190843f012562643e325
timur.guliev <timur.guliev@gmail.com>
2024-11-24 18:13:51 +0500

    add module_13_3

---
+++ module13/Методы отправки сообщений/module_13_3.py
@@ -0,0 +1,31 @@
+from aiogram import Bot, Dispatcher, executor, types 
+from aiogram.contrib.fsm_storage.memory import MemoryStorage 
+import asyncio
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher(bot, storage= MemoryStorage())
+
+# @dp.message_handler(text = ['Urban', 'ff']) 
+# async def urban_message(message):
+#     print("Urban message")
+    
+@dp.message_handler(commands=['start']) 
+async def start_message(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+    # print("Привет! Я бот помогающий твоему здоровью.")
+    
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+    # print("Введите команду /start, чтобы начать общение.")
+    
+    
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
