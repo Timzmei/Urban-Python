698d52f3ae77a8fdeb13dc4044fdab7c1015620c
timur.guliev <timur.guliev@gmail.com>
2024-11-26 02:05:22 +0500

    add mdule_14_2

---
+++ module14/Выбор элементов и функции в SQL запросах/module_14_2.py
@@ -0,0 +1,49 @@
+import sqlite3
+
+connection = sqlite3.connect('module14\Создание БД, добавление, выбор и удаление элементов\\not_telegram.db')
+
+cursor = connection.cursor()
+
+cursor.execute('''
+               CREATE TABLE IF NOT EXISTS Users(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   username TEXT UNIQUE NOT NULL,
+                   email TEXT UNIQUE NOT NULL,
+                   age INTEGER,
+                   balance INTEGER NOT NULL)''')
+
+cursor.execute('''
+               CREATE INDEX IF NOT EXISTS index_email ON Users(email)''')
+
+cursor.execute("DELETE FROM Users")
+
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users(username, email, age, balance) VALUES(?, ?, ?, ?)', (f'User{i}', f'example{i}@gmail.com', 10 * i, 1000))
+
+for i in range(1, 11, 2):
+    cursor.execute(f"UPDATE Users SET balance = ? WHERE username = ?", (500, f'User{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute(f'DELETE FROM Users WHERE username = ?', (f'User{i}',))
+    
+cursor.execute(f'DELETE FROM Users WHERE id = ?', (f'6',))  
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+print(cursor.fetchone()[0])
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+print(cursor.fetchone()[0])
+
+cursor.execute("SELECT AVG(balance) FROM Users")
+print(cursor.fetchone()[0])
+
+connection.commit()
+
+# cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
+
+# results = cursor.fetchall()
+# for user in results:
+#     print(f"Имя: {user} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}")
+
+
+connection.close()
