85b0e2f5b6d6e9094b78c8d189c49d6f98252256
timur.guliev <timur.guliev@gmail.com>
2025-01-09 00:04:00 +0500

    update

--- module16/CRUD Запросы Get Post Put Delete/module_16_3.py
+++ module16/CRUD Запросы Get Post Put Delete/module_16_3.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Path, HTTPException
 
 app = FastAPI()
 
@@ -10,22 +10,29 @@ def read_users():
     return users
 
 @app.post("/user/{username}/{age}")
-def create_user(username: str, age: int):
+def create_user(
+    username: str = Path(..., title="Enter username", min_length=5, max_length=20, description="Enter username", example="UrbanUser"),
+    age: int = Path(..., title="Enter age", ge=18, le=120, description="Enter age", example=24)
+):
     global user_id
     user_id += 1
     users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 @app.put("/user/{user_id}/{username}/{age}")
-def update_user(user_id: str, username: str, age: int):
-    if user_id not in users:
+def update_user(
+    user_id: int = Path(..., title="Enter User ID", ge=1, le=100, description="Enter User ID", example=1),
+    username: str = Path(..., title="Enter username", min_length=5, max_length=20, description="Enter username", example="UrbanProfi"),
+    age: int = Path(..., title="Enter age", ge=18, le=120, description="Enter age", example=28)
+):
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail="User not found")
-    users[user_id] = f"Имя: {username}, возраст: {age}"
+    users[str(user_id)] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 @app.delete("/user/{user_id}")
-def delete_user(user_id: str):
-    if user_id not in users:
+def delete_user(user_id: int = Path(..., title="Enter User ID", ge=1, le=100, description="Enter User ID", example=1)):
+    if str(user_id) not in users:
         raise HTTPException(status_code=404, detail="User not found")
-    del users[user_id]
+    del users[str(user_id)]
     return f"User {user_id} has been deleted"
